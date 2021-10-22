435eee7e96f1a7450eef78bb61a83b6233a93927
BlankR <hjyblanche@gmail.com>
2021-10-21 21:16:07 -0400

    chaged route

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 pip3 install -r requirements.txt
-python3 manage.py runserver 0.0.0.0:8000
-python3 manage.py makemigrations
-python3 manage.py migrate
+python3 react1/manage.py runserver 0.0.0.0:8000
+python3 react1/manage.py makemigrations
+python3 react1/manage.py migrate
